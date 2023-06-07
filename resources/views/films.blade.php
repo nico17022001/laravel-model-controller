c64e5835e9258224c928fbe9d42376b7785e6335
nico17022001 <nico17022001boolean@gmail.com>
2023-06-07 18:04:34 +0200

    corretti elenco film

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -7,10 +7,6 @@
     <thead>
         <tr>
             <th scope="col">Nome</th>
-            <th scope="col">Nome Originale</th>
-            <th scope="col">Nazionalità</th>
-            <th scope="col">Data di uscita</th>
-            <th scope="col">Voto</th>
             <th scope="col">Dettagli film</th>
         </tr>
     </thead>
@@ -20,10 +16,6 @@
 
             <tr>
                 <td>{{ $film->title }}</td>
-                <td>{{ $film->original_title}}</td>
-                <td>{{ $film->nationality}}</td>
-                <td>{{ $film->date}}</td>
-                <td>{{ $film->vote}}</td>
                 <td>
                     <a href="{{route('film-detail', ['id' => $film->id])}}" class="btn btn-primary">Info</a>
                 </td>
